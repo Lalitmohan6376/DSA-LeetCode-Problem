bfe081cbb4dd07ba2378ee4a626b94c6232180ac
Lalit_mohan <lalitg6376@gmail.com>
2026-03-13 17:53:39 +0530

    Update Range_sum.py

--- Range_sum.py
+++ Range_sum.py
@@ -5,7 +5,6 @@
 
 # NumArray(int[] nums) Initializes the object with the integer array nums.
 # int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
- 
 
 
 class NumArray:
@@ -28,3 +27,4 @@ obj = NumArray(nums)
 print(obj.sumRange(0, 2))  
 print(obj.sumRange(2, 5))  
 print(obj.sumRange(0, 5))  
+
