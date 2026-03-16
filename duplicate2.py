3b3d09efecb3d73389965efc35e958fef84a6cc5
Lalit_mohan <lalitg6376@gmail.com>
2026-03-16 14:53:10 +0530

    Update duplicate2.py

--- duplicate2.py
+++ duplicate2.py
@@ -4,7 +4,6 @@
 # Companies
 # Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
- 
 
 # Example 1:
 
