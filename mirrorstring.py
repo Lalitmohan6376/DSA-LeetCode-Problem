a2c1e82bff2f935e0bfe7d09d9c9f0e1e1048c4b
Lalit_mohan <lalitg6376@gmail.com>
2026-03-17 16:31:50 +0530

    Create mirrorstring.py

---
+++ mirrorstring.py
@@ -0,0 +1,11 @@
+def mirrorstring(s):
+    count = 0
+    n = len(s)
+    
+    for i in range(0,n):
+        for j in range(i+1,n):
+            if s[i] == s[j]:
+                count+=1
+    return count
+        
+print(mirrorstring("abba"))
