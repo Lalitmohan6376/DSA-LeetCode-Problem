2ad06d8aeec491b2d07a050a51c51f3734d46bec
Lalit_mohan <lalitg6376@gmail.com>
2026-03-24 19:39:43 +0530

    Update pivotIndex.py

--- pivotIndex.py
+++ pivotIndex.py
@@ -4,7 +4,6 @@ class Solution:
         left_sum = 0
         for i in range(len(nums)):
             right_sum = total-left_sum-nums[i]
-
             if left_sum == right_sum:
                 return i
 
