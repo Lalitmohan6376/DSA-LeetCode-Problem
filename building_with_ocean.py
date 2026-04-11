d579cce5a55685e987719cc90408e10955c73464
Lalit_mohan <lalitg6376@gmail.com>
2026-04-11 14:38:01 +0530

    Create building_with_ocean.py

---
+++ building_with_ocean.py
@@ -0,0 +1,16 @@
+def maxheight(h):
+    n = len(h)
+    result = []
+    max_height = -1
+    i = n-1
+    
+    while i >= 0:
+        if h[i] > max_height:
+            result.insert(0,i)
+            
+            max_height = h[i]
+        i-=1
+    return result
+    
+    
+print(maxheight([4,2,3,1]))
