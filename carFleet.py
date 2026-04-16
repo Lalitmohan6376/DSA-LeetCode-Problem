64cdccd62e063d4926b35e24795025b8472819af
Lalit_mohan <lalitg6376@gmail.com>
2026-04-16 09:05:40 +0530

    Create carFleet.py

---
+++ carFleet.py
@@ -0,0 +1,22 @@
+class Solution:
+    def carFleet(self, target, position, speed):
+        car = list(zip(position,speed))
+        car.sort(reverse=True)
+        
+        stack = []
+        
+        for pos,spd in car:
+            time = (target-pos) / spd
+            
+            if not stack or time > stack[-1]:
+                stack.append(time)
+        
+        return len(stack)
+
+obj = Solution()
+target = 12
+position = [10,8,0,5,3]
+speed = [2,4,1,1,3]
+
+ans = obj.carFleet(target,position,speed)
+print(ans)
