bfda30120b43c70e7be5826bd71c1c2221660bc6
Lalit_mohan <lalitg6376@gmail.com>
2026-04-20 20:13:15 +0530

    Update Two_sum.py

--- Two_sum.py
+++ Two_sum.py
@@ -1,7 +1,26 @@
 class Solution:
     def twoSum(self, nums, target):
+        arr = []
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-Solution().twoSum([2, 7, 11, 15], 9)
+            arr.append((nums[i], i))
+        
+        arr.sort()
+        left = 0
+        right = len(nums) - 1
+        
+        while left < right:
+            curr_sum = arr[left][0] + arr[right][0]
+            
+            if curr_sum == target:
+                return [arr[left][1], arr[right][1]]
+            
+            elif curr_sum < target:
+                left += 1
+            else:
+                right -= 1
+
+
+
+obj = Solution()
+result = obj.twoSum([2, 7, 11, 15], 9)
+print(result)
