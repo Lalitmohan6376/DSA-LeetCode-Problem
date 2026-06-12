c0403afecc11649ad1f7dd6266c89c7e3c3f11b7
Lalit_mohan <lalitg6376@gmail.com>
2026-06-12 21:49:40 +0530

    Create Merge_two_sorted_linked_list.py

---
+++ Linked List/Merge_two_sorted_linked_list.py
@@ -0,0 +1,46 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def mergeTwoLists(list1, list2):
+
+    dummy = ListNode(0)
+    tail = dummy
+
+    while list1 and list2:
+
+        if list1.val <= list2.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+
+        tail = tail.next
+
+    if list1:
+        tail.next = list1
+
+    if list2:
+        tail.next = list2
+
+    return dummy.next
+
+
+# List 1: 1 -> 2 -> 4
+list1 = ListNode(1)
+list1.next = ListNode(2)
+list1.next.next = ListNode(4)
+
+# List 2: 1 -> 3 -> 4
+list2 = ListNode(1)
+list2.next = ListNode(3)
+list2.next.next = ListNode(4)
+
+result = mergeTwoLists(list1, list2)
+
+while result:
+    print(result.val, end=" ")
+    result = result.next
