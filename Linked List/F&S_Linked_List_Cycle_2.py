d325fd497c1f104c0869d14c23834d0b20a0f336
Lalit_mohan <lalitg6376@gmail.com>
2026-06-20 10:00:21 +0530

    Create F&S_Linked_List_Cycle_2.py

---
+++ Linked List/F&S_Linked_List_Cycle_2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def detectCycle(self, head):
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+        else:
+            return None
+
+        curr = head
+        while curr != slow:
+            curr = curr.next
+            slow = slow.next
+
+        return curr
