67c75aaf4b2ec6e09c18b43aafbad01e5b292aea
Lalit_mohan <lalitg6376@gmail.com>
2026-07-03 08:45:32 +0530

    Create Inorder.py

---
+++ Binary-Tree/Inorder.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def __init__(self):
+       self.ans = []
+
+    def Inorder(self,root):
+      # Base case
+      if root is None:
+        return
+      
+      
+      self.Inorder(root.left)
+      self.ans.append(root.val)
+      self.Inorder(root.right)
+
+    def inorderTraversal(self, root):
+      self.ans = []
+      self.Inorder(root)
+      return self.ans
+    
+        
