29c1996d20d5e355c9ba3a8c1df88998ccf361a3
Lalit_mohan <lalitg6376@gmail.com>
2026-07-10 14:53:47 +0530

    Create Balanced_tree_DFS.py

---
+++ Binary-Tree/Balanced_tree_DFS.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def check(self, root):
+        if root is None:
+            return 0
+
+        left = self.check(root.left)
+        if left == -1:
+            return -1
+
+        right = self.check(root.right)
+        if right == -1:
+            return -1
+
+        if abs(left - right) > 1:
+            return -1
+
+        return max(left, right) + 1
+
+    def isBalanced(self, root):
+        return self.check(root) != -1
+
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+obj = Solution()
+print(obj.isBalanced(root))
