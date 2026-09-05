399da1f247899e547ad2e37e13d981295bd1d974
Lalit_mohan <lalitg6376@gmail.com>
2026-09-05 21:53:44 +0530

    Update Delete Node in a BST.py

--- Binary Search Tree/Delete Node in a BST.py
+++ Binary Search Tree/Delete Node in a BST.py
@@ -36,3 +36,39 @@ class Solution:
             return node
 
         return buildTree(0, len(values) - 1)
+
+#optimal solution
+
+class Solution:
+    def deleteNode(self, root, key):
+
+        if root is None:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+
+            if root.left is None and root.right is None:
+                return None
+
+    
+            if root.left is None:
+                return root.right
+
+            if root.right is None:
+                return root.left
+
+            current = root.right
+
+            while current.left is not None:
+                current = current.left
+
+            root.val = current.val
+
+            root.right = self.deleteNode(root.right, current.val)
+
+        return root
